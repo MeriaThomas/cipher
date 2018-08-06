74dfc3590f8ef8458ad310fc3f12baef5f316b37
Meria Thomas and Skye Nguyen <skye@>
2018-08-06 12:44:22 -0700

    add bootstrap stylings

--- js/scripts.js
+++ js/scripts.js
@@ -1,36 +1,43 @@
-var mySentence = prompt("Enter a sentence.");
+$(function(){
+  var mySentence = prompt("Enter a sentence.");
 
-alert(mySentence);
+  $("#before").click(function() {
+    alert(mySentence);
+  })
 
-function firstLast(mySentence) {
-  var firstLetter = mySentence.charAt(0);
-  var lastLetter = mySentence.charAt(mySentence.length-1);
-	return (firstLetter+lastLetter).toUpperCase();
+  $("#after").click(function() {
+    function firstLast(mySentence) {
+      var firstLetter = mySentence.charAt(0);
+      var lastLetter = mySentence.charAt(mySentence.length-1);
+    	return (firstLetter+lastLetter).toUpperCase();
+    }
+
+    function lastFirst(firstLast) {
+      var firstLetter = firstLast(mySentence).charAt(0);
+      var lastLetter = firstLast(mySentence).charAt(firstLast(mySentence).length-1);
+    	return (lastLetter+firstLetter).toUpperCase();
+    }
+
+
+    function combineFunction(firstLast,lastFirst) {
+      var secondFunctionResult = lastFirst(firstLast);
+      return mySentence+secondFunctionResult;
+    }
+
+    function counterConcat(mySentence) {
+      var result = (mySentence.length/2);
+      return mySentence.charAt(result) + combineFunction(firstLast,lastFirst);
+    }
+
+    function finalReverseFunction(counterConcat) {
+      var splitStr = counterConcat(mySentence).split("");
+      var splitArray = splitStr.reverse();
+      var joinArray = splitArray.join("");
+      return joinArray;
+    }
+    alert(finalReverseFunction(counterConcat));
+  }
+  )
+
 }
-
-// alert(firstLast(mySentence));
-
-function lastFirst(firstLast) {
-  var firstLetter = firstLast(mySentence).charAt(0);
-  var lastLetter = firstLast(mySentence).charAt(firstLast(mySentence).length-1);
-	return (lastLetter+firstLetter).toUpperCase();
-}
-
-
-function combineFunction(firstLast,lastFirst) {
-  var secondFunctionResult = lastFirst(firstLast);
-  return mySentence+secondFunctionResult;
-}
-
-function counterConcat(mySentence) {
-  var result = (mySentence.length/2);
-  return mySentence.charAt(result) + combineFunction(firstLast,lastFirst);
-}
-
-function finalReverseFunction(counterConcat) {
-  var splitStr = counterConcat(mySentence).split("");
-  var splitArray = splitStr.reverse();
-  var joinArray = splitArray.join("");
-  return joinArray;
-}
-alert(finalReverseFunction(counterConcat));
+);
